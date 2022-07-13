5b0b8a00e0e4197aae5831ae1a3dc68af580dbd0
Sevgi <sevgi2308@gmail.com>
2022-07-13 11:50:29 +0300

    first tests added

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -3,3 +3,7 @@ import sum from "./skeleton";
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
 });
+test("sum adds two numbers", () => {
+  expect(sum(5, 5)).toBe(10);
+  expect(sum(-10, 3)).toBe(-7);
+});
